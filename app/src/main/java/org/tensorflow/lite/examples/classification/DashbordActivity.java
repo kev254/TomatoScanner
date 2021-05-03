58990cdbefa76f984d323cb88f23d8fa8003de59
Kevoh <40100329+kev254@users.noreply.github.com>
2021-05-03 17:09:26 +0300

    Second commit

--- app/src/main/java/org/tensorflow/lite/examples/classification/DashbordActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/DashbordActivity.java
@@ -27,88 +27,22 @@ import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
 public class DashbordActivity extends AppCompatActivity {
-    FloatingActionButton mAddFab, mAddAlarmFab, mAddPersonFab;
 
-    // These are taken to make visible and invisible along
-    // with FABs
-    TextView addAlarmActionText, addPersonActionText;
-
-    // to check whether sub FAB buttons are visible or not.
-    Boolean isAllFabsVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashbord);
-        Toolbar toolbar=findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        if (getSupportActionBar()!=null){
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle("");
         }
-        mAddFab = findViewById(R.id.add_fab);
-
-        mAddAlarmFab = findViewById(R.id.add_alarm_fab);
-        mAddPersonFab = findViewById(R.id.add_person_fab);
-        addAlarmActionText = findViewById(R.id.add_alarm_action_text);
-        addPersonActionText = findViewById(R.id.add_person_action_text);
-
-        mAddAlarmFab.setVisibility(View.GONE);
-        mAddPersonFab.setVisibility(View.GONE);
-        addAlarmActionText.setVisibility(View.GONE);
-        addPersonActionText.setVisibility(View.GONE);
-
-        isAllFabsVisible = false;
-
-        mAddFab.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
-                        if (!isAllFabsVisible) {
-
-
-                            mAddAlarmFab.show();
-                            mAddPersonFab.show();
-                            addAlarmActionText.setVisibility(View.VISIBLE);
-                            addPersonActionText.setVisibility(View.VISIBLE);
-                            isAllFabsVisible = true;
-                            mAddFab.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_multiply));
-                        } else {
-
-
-                            mAddAlarmFab.hide();
-                            mAddPersonFab.hide();
-                            addAlarmActionText.setVisibility(View.GONE);
-                            addPersonActionText.setVisibility(View.GONE);
-                            isAllFabsVisible = false;
-                            mAddFab.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_add));
-
-                        }
-                    }
-                });
-
-
-        mAddPersonFab.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        //Toast.makeText(DashbordActivity.this, "Person Added", Toast.LENGTH_SHORT).show();
-                        Intent i = new Intent(DashbordActivity.this, ClassifierActivity.class);
-                        startActivity(i);
-                    }
-                });
-
-
-        mAddAlarmFab.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Toast.makeText(DashbordActivity.this, "This activity is not yet added", Toast.LENGTH_SHORT).show();
-
-                    }
-                });
     }
+
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu,menu);
@@ -119,8 +53,12 @@ public class DashbordActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        if (item.getItemId()==R.id.scan){
+//            Intent i = new Intent(DashbordActivity.this, ClassifierActivity.class);
+//            startActivity(i);
+//        }
         if (item.getItemId()==R.id.scan){
-            Intent i = new Intent(DashbordActivity.this, ClassifierActivity.class);
+            Intent i = new Intent(DashbordActivity.this, MenuActivity.class);
             startActivity(i);
         }
         if (item.getItemId()==R.id.btn_logout){
---
+++ app/src/main/java/org/tensorflow/lite/examples/classification/RecyclerViewOnclickListener.java
@@ -0,0 +1,8 @@
+package org.tensorflow.lite.examples.classification;
+
+import android.view.View;
+
+public interface RecyclerViewOnclickListener {
+
+    void onClick(View view, int position);
+}
